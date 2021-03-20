845c15ce15d5b7cbecda14dca24c99850e3fd780
TOWER\ptyagi <piyush.tyagi@tal.com.au>
2021-03-20 20:33:44 +1100

    added Infra and static pages search.

---
+++ InfotrackSearch/Application/BusinessLogic/StaticPagesSearch.cs
@@ -0,0 +1,16 @@
+﻿using Application.Interface;
+using Application.Queries.GetSearchResult;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.BusinessLogic
+{
+    public class StaticPagesSearch : IStaticPagesSearch
+    {
+        public SearchResultViewModel Search(string searchQuery)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InfotrackSearch/Infrastructure/StaticSearch.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class StaticSearch
+    {
+        private readonly IConfiguration _configuration;
+        public StaticSearch(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string Search(string url)
+        {
+            WebClient webClient = new WebClient();
+            webClient.Headers.Add("user-agent", "Infotrach search");
+            return webClient.DownloadString(url);
+        }
+    }
+}
